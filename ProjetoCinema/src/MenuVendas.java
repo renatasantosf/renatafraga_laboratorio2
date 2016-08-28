f6f98ee3f7b246f2275c9b02d58048f519093b2d
Renata Fraga <renatasfraga@gmail.com>
2016-08-28 11:07:46 -0300

    Insercao de projeto Cinema no repositorio. Ate agora foram realizadas apenas a criacao do projeto e a classe principal.

---
+++ ProjetoCinema/src/MenuVendas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author renat
+ */
+public class MenuVendas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
