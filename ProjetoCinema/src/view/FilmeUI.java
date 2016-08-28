5aea72b243aa47c8e532ed278867da7196407b27
Renata Fraga <renatasfraga@gmail.com>
2016-08-28 17:00:09 -0300

    Insercao da classe FilmeUI e metodos construtor e iniciar()

---
+++ ProjetoCinema/src/view/FilmeUI.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import repositorio.RepositorioFilme;
+
+/**
+ *
+ * @author renat
+ */
+public class FilmeUI {
+    
+    private RepositorioFilme listaFilmes;
+    
+    public FilmeUI(RepositorioFilme lista) {
+        this.listaFilmes = lista;
+    }
+    
+   //TODO: menu em outra classe
+    
+    public void iniciar() {
+        byte opcao = 0;
+        while (opcao!=5) {
+            
+        }
+    }
+}
